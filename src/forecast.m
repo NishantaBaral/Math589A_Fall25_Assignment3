c436be279b72a227935c626ac4e99452c71b9fd5
Nishanta Baral <nishantabaral48@gmail.com>
2025-11-09 13:18:14 -0700

    fixed bugs

--- src/forecast.m
+++ src/forecast.m
@@ -1,24 +1,23 @@
 function yF = forecast(y, s, coef, H)
-y = y(:); T = numel(y); N = numel(coef.a); K = numel(coef.alpha);
-yF = zeros(H,1);
-for h = 1:H
-    t = T + h;                                % absolute time for BOTH trend and season
-    % season
-    sea = 0;
-    for k = 1:K
-        sea = sea + coef.alpha(k)*cos(2*pi*k*t/s) + coef.beta(k)*sin(2*pi*k*t/s);
-    end
-    % intercept + trend (absolute t)
-    acc = coef.c + coef.d*t + sea;
-    % AR lags
-    for i = 1:N
-        idx = t - i;                          % y_{t-i}
-        if idx <= T
-            acc = acc + coef.a(i)*y(idx);     % use actual data
-        else
-            acc = acc + coef.a(i)*yF(idx - T);% use prior forecasts
+    y = y(:); T = numel(y);
+    N = numel(coef.a); K = numel(coef.alpha);
+    if ~isfield(coef, 'beta'), coef.beta = zeros(size(coef.alpha)); end
+    yF = zeros(H,1);
+    history = [y; zeros(H,1)];
+    for h = 1:H
+        t = T + h;
+        sea = 0;
+        for k = 1:K
+            sea = sea + coef.alpha(k)*cos(2*pi*k*t/s) + coef.beta(k)*sin(2*pi*k*t/s);
         end
+        acc = coef.c + coef.d*t + sea;
+        for i = 1:N
+            tau = t - i;
+            if tau >= 1
+                acc = acc + coef.a(i) * history(tau);
+            end
+        end
+        history(T + h) = acc;
+        yF(h) = acc;
     end
-    yF(h) = acc;
-end
 end
