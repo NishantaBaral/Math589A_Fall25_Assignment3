77f497c30b367fb44514b1a2c644d5fe82a9fd4a
Nishanta Baral <nishantabaral48@gmail.com>
2025-11-09 00:52:31 -0700

    fixed bugs

--- src/forecast.m
+++ src/forecast.m
@@ -1,21 +1,22 @@
 function yF = forecast(y, s, coef, H)
 y = y(:); T = numel(y); N = numel(coef.a); K = numel(coef.alpha);
-M = T - N;
 yF = zeros(H,1);
 for h = 1:H
-    t = T + h;                 % absolute time for season/lag
-    r = M + h;                 % row-index for trend
+    t = T + h;                                % absolute time for BOTH trend and season
+    % season
     sea = 0;
     for k = 1:K
         sea = sea + coef.alpha(k)*cos(2*pi*k*t/s) + coef.beta(k)*sin(2*pi*k*t/s);
     end
-    acc = coef.c + coef.d*r + sea;     % trend uses r
+    % intercept + trend (absolute t)
+    acc = coef.c + coef.d*t + sea;
+    % AR lags
     for i = 1:N
-        idx = t - i;
+        idx = t - i;                          % y_{t-i}
         if idx <= T
-            acc = acc + coef.a(i) * y(idx);
+            acc = acc + coef.a(i)*y(idx);     % use actual data
         else
-            acc = acc + coef.a(i) * yF(idx - T);
+            acc = acc + coef.a(i)*yF(idx - T);% use prior forecasts
         end
     end
     yF(h) = acc;
